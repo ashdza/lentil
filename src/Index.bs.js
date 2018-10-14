98aa883bc665aa9b38e8369e52f87ef6aca2549e
Ashley <ash.1303.github@dsouzaville.com>
2018-10-14 00:52:49 -0500

    Fix version of reason-react

--- src/Index.bs.js
+++ src/Index.bs.js
@@ -3,10 +3,10 @@
 var Belt_Debug = require("bs-platform/lib/js/belt_Debug.js");
 var ReactDOMRe = require("reason-react/src/ReactDOMRe.js");
 var ReasonReact = require("reason-react/src/ReasonReact.js");
-var AppDemo$LentilLite = require("./AppDemo.bs.js");
+var AppDemo$Lentil = require("./AppDemo.bs.js");
 
 Belt_Debug.setupChromeDebugger(/* () */0);
 
-ReactDOMRe.renderToElementWithId(ReasonReact.element(undefined, undefined, AppDemo$LentilLite.make(/* array */[])), "target");
+ReactDOMRe.renderToElementWithId(ReasonReact.element(undefined, undefined, AppDemo$Lentil.make(/* array */[])), "target");
 
 /*  Not a pure module */
