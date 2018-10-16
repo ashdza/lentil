5e12a0d34edb024c19d8b698c54cc297d9755f4a
Ashley <ash.1303.github@dsouzaville.com>
2018-10-16 10:59:41 -0500

    Add the little icon (favicon) to appear in browser tabs

--- dist/styles.5072464b.js
+++ dist/styles.5072464b.js
@@ -171,7 +171,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"css/styles.css":[function(require,module,exports) {
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"assets/css/styles.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57117" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58210" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
