75368445464ae071e1a9e6ec405a797906874b1d
Ashley <ash.1303.github@dsouzaville.com>
2018-10-27 18:43:36 -0500

    set preferred fonts

--- dist/Index.3930469b.js
+++ dist/Index.3930469b.js
@@ -45937,7 +45937,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50320" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54497" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
